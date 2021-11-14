1e5015ccad6c52fe3d9682e2a9c886f3008c0410
TGUXX <43382783+MAINcsgo@users.noreply.github.com>
2021-11-14 13:10:57 +0100

    Update bazosbot.py

--- bazosbot.py
+++ bazosbot.py
@@ -8,121 +8,73 @@ counter = 0
 tmp_list = []
 
 
-#EMAIL LOGIN (gmail)
-email =  'insert sender email here'
-password = 'insert sender email password here'
-receiver = 'insert receiver email address here'
-
 #SPACER
 def clear_line():
     for i in range(1,5):
         print('')
-
+  
+                
 print('************************************************************')
 print('* ____           __________   _____    ____   ____ _______ *')
 print('*|  _ \   /\    |___  / __ \ / ____|  |  _ \ / __ \__   __|*')
-print('*| |_) | /  \      / / |  | | (___    | |_) | |  | | | |   *'   )   
+print('*| |_) | /  \      / / |  | | (___    | |_) | |  | | | |   *')   
 print('*|  _ < / /\ \    / /| |  | |\___ \   |  _ <| |  | | | |   *')   
 print('*| |_) / ____ \  / /_| |__| |____) |  | |_) | |__| | | |   *')   
 print('*|____/_/    \_\/_____\____/|_____/   |____/ \____/  |_|   *')  
 print('*                                                          *')
+print('*                    [For GPU SCOUTING]                    *')
 print('************************************************************')
 print('')
+print('CTRL+C to terminate the script')
+print('')
 
-
-#REFRESHING
+#SETUP 
 refresh = input('REFRESH (secs/enter=60s): ')
 if refresh == '':   
     refresh = 60
     
-      
-#CATEGORY
-print("PC / MOBILE / CAR: ")
-cat = input(': ')
+    
+alert_opt = input("Do you wish to get email alerts?  (y/n): ")
+
+if (alert_opt == 'y') or (alert_opt == 'Y'):
+    email = input("Enter your E-mail (gmail only!): ")
+    password = input("Enter password for your : ")
+    
+else:
+    pass
 
 
-if (cat == 'PC') or (cat =='pc'):
+component = 'graficka';
+cena_od = input('CENA OD: ')
+cena_do = input('CENA DO: ') 
+baseurl = f"https://pc.bazos.sk/{component}/?hledat=&rubriky=pc&hlokalita=&humkreis=25&cenaod={cena_od}&cenado={cena_do}&Submit=H%C4%BEada%C5%A5&kitx=ano"    
 
-    print("cpu / gpu / ram / mobo")
-    component = input(': ')
-    
-    if component == 'cpu':
-        component = 'procesor'
-        
-    if component == 'gpu':
-        component = 'graficka'
-        
-    if component == 'ram':
-       component = 'pamet'    
-   
-    if component == 'mobo':
-        component = 'motherboard' 
-        
-    cena_od = input('CENA OD: ')
-    cena_do = input('CENA DO: ') 
-    baseurl = f"https://pc.bazos.sk/{component}/?hledat=&rubriky=pc&hlokalita=&humkreis=25&cenaod={cena_od}&cenado={cena_do}&Submit=H%C4%BEada%C5%A5&kitx=ano"    
-        
-     
-        
-if   (cat =='mobile') or (cat =='MOBILE'):
-    
-    print("ALL / APPLE / SAMSUNG / xx")
-    component = input(': ')
-    
-    if (component == 'ALL') or (component == 'all'):
-        component = ''
-        
-    if (component == 'APPLE') or (component == 'apple'):
-        component = 'apple/'
-        
-    if (component == 'SAMSUNG') or (component == 'samsung'):
-        component = 'samsung/'   
-     
-        
-    cena_od = input('CENA OD: ')
-    cena_do = input('CENA DO: ') 
-    baseurl = f"https://mobil.bazos.sk/{component}?hledat=&rubriky=mobil&hlokalita=&humkreis=25&cenaod={cena_od}&cenado={cena_do}&Submit=H%C4%BEada%C5%A5&kitx=ano"
-   
-    
-if   (cat =='car') or (cat =='CAR'):
-    
-    print("ALL / x / x / x")
-    component = input(': ')
-    
-    if component == ('ALL' or 'all'):
-        component = ''
-        
- 
-    cena_od = input('CENA OD: ')
-    cena_do = input('CENA DO: ') 
-    baseurl = f'https://auto.bazos.sk/?hledat=&rubriky=auto&hlokalita=&humkreis=25&cenaod={cena_od}&cenado={cena_do}&Submit=H%C4%BEada%C5%A5&kitx=ano'
-    
-  
-ALERT = f'MARKET CHANGE DETECTED/n {baseurl} '     
+baseurl_test = "https://pc.bazos.sk/"+component+"/?hledat=&rubriky=pc&hlokalita=&humkreis=25&cenaod="+cena_od+"&cenado="+cena_od+"&Submit=H%C4%BEada%C5%A5&kitx=ano"    
 
 #EMAIL SENDING
 def send_alert():
     
+
     mail = smtplib.SMTP('smtp.gmail.com',587)
     mail.ehlo()
     mail.starttls()
     mail.login(email,password)
-    mail.sendmail(email,receiver,ALERT)
+    mail.sendmail(email,email,ALERT)
     mail.close()   
     print("******** EMAIL WAS SENT ********")
     
 
+#HEADERS
 headers = {   
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'
     }
 
-alerts = 0
 
+alerts = 0
 
 while True:
     
-    clear_line()
-    
+    clear_line() 
     GPU_list = []
     counter += 1
     
@@ -138,9 +90,10 @@ while True:
     
     for item in product_list:
         
-        name = item.find('span',class_='nadpis').text.strip()
+        name =  item.find('h2',class_='nadpis').text.strip()
         price = item.find('div',class_='inzeratycena').text.strip()
-        date = item.find('span',class_='velikost10').text.strip()
+        date =  item.find('span',class_='velikost10').text.strip()
+        link = item.find('a').text.strip()
     
         GPU = {
                      
@@ -153,9 +106,20 @@ while True:
         GPU_list.append(GPU)
         
     if GPU_list != tmp_list: 
-        alerts += 1 
-       
         
+        if (alert_opt == 'y') or (alert_opt == 'Y'):
+            
+             if (alerts == 0):
+      
+                ALERT = "BAZOSBOT activated and WORKING!"    
+                send_alert()                
+             else:
+                 
+                 ALERT = "Market change detected!"             
+                 alerts += 1
+                 send_alert()
+      
+      
     tmp_list = GPU_list
  
     df = pd.DataFrame(GPU_list)
